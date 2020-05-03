b229ce886fca278d4bce9623bb892bc9a4ee4214
Austine Gomez <austine.gomez@yahoo.com>
2020-05-03 09:30:03 -0500

    edit

--- public/script.js
+++ public/script.js
@@ -1,208 +1,201 @@
+var socket = io();
 var user;
- var socket = io();
+ 
+//intro login screen
 function usernameAsk() {
-  $(".grey-out").fadeIn(500);
-  $(".user").fadeIn(500);
-  $(".user").submit(function () {
-    event.preventDefault();
-    user = $("#username").val().trim();
+    $('.grey-out').fadeIn(500);
+    $('.user').fadeIn(500);
+    $('.user').submit(function(){
+        event.preventDefault();
+        user = $('#username').val().trim();
+        if (user == '') {
+            return false
+        };
 
-    if (user == "") {
-      return false;
+        var index = users.indexOf(user);
+        if (index > -1) {
+            alert(user + ' already exists');
+            return false
+        };
+        
+        socket.emit('join', user);
+        $('.grey-out').fadeOut(300);
+        $('.user').fadeOut(300);
+        $('input.guess-input').focus();
+    });
+};
+
+var context;
+var canvas;
+var click = false;
+
+//clear screen
+var clearScreen = function() {
+    context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+};
+
+//function for handling players guess
+var guesser = function() {
+    clearScreen();
+    click = false;
+    console.log('draw status: ' + click);
+    $('.draw').hide();
+    $('#guesses').empty();
+    console.log('You are a guesser');
+    $('#guess').show();
+    $('.guess-input').focus();
+
+    $('#guess').on('submit', function() {
+        event.preventDefault();
+        var guess = $('.guess-input').val();
+
+        if (guess == '') {
+            return false
+        };
+
+        console.log(user + "'s guess: " + guess);
+        socket.emit('guessword', {username: user, guessword: guess});
+        $('.guess-input').val('');
+    });
+};
+
+//guess word
+var guessword = function(data){
+    $('#guesses').text(data.username + "'s guess: " + data.guessword);
+
+    if (click == true && data.guessword == $('span.word').text() ) {
+        console.log('guesser: ' + data.username + ' draw-word: ' + $('span.word').text());
+        socket.emit('correct answer', {username: data.username, guessword: data.guessword});
+        socket.emit('swap rooms', {from: user, to: data.username});
+        click = false;
     }
-    $("#users").append(user);
-    //console.log(user);
-    // if (index > -1) {
-    //     alert(user + ' already exists');
-    //     return false
-    // };
+};
 
-    $(".grey-out").fadeOut(300);
-    $(".user").fadeOut(300);
-  });
-}
+//retreive word
+var wordToDraw = function(word) {
+    $('span.word').text(word);
+    console.log('Your word to draw is: ' + word);
+};
 
 
-function clearCanvas() {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  console.log("clearing called");
-}
-
-
-//function for perosn guessing
-var guesser = function(){
-}
-
-//function for displaying word
-var guessWord = function(data){
-
-}
-
-//display list of users playing
+//display players names
 var users = [];
-var userList = function(names){
-users = names;
-}
+var userlist = function(names) {
+    users = names;
+    var html = '<p>' + 'Players' + '</p>';
+    for (var i = 0; i < names.length; i++) {
+        html += '<li>' + names[i] + '</li>';
+    };
+    $('ul').html(html);
+};
 
-//new player drawer
-var newDrawer =  function(){
+//new drawer
+var newDrawer = function() {
+    socket.emit('new drawer', user);
+    clearScreen();
+    $('#guesses').empty();
+};
 
-}
+//player answers correctly
+var correctAnswer = function(data) {
+    $('#guesses').html('<p>' + data.username + ' guessed correctly!' + '</p>');
+};
 
-//suplay when corrct answer is given
-var correctAnswer = function(data){
+//rest for new game 
+var reset = function(name) {
+    clearScreen();
+    $('#guesses').empty();
+    console.log('New drawer: ' + name);
+    $('#guesses').html('<p>' + name + ' is the new drawer' + '</p>');
+};
 
-}
-
-//reset page with new drawer
-var reset  = function(name){
-
-}
-
-
-
-var word = new Array();
-word[0] = "dinasour";
-word[1] = "automobile";
-word[2] = "cat";
-word[3] = "dog";
-word[4] = "word";
-word[5] = "letter";
-word[6] = "number";
-word[7] = "person";
-word[8] = "pen";
-word[9] = "police";
-word[10] = "people";
-word[11] = "sound";
-word[12] = "water";
-word[13] = "breakfast";
-word[14] = "place";
-word[15] = "man";
-word[16] = "army";
-word[17] = "woman";
-word[18] = "oven";
-word[19] = "boy";
-word[20] = "girl";
-word[21] = "ghost";
-word[22] = "fire hydrant";
-word[23] = "world";
-word[24] = "month";
-word[25] = "newspaper";
-word[26] = "sentence";
-word[27] = "line";
-word[28] = "telvision";
-var intWordIndex = 0;
-
-// //Generate new word
-function newWord() {
-  intWordIndex = word[Math.floor(Math.random() * word.length)];
-  document.getElementById("myWord").innerHTML = intWordIndex;
- clearCanvas();
-}
-// retrive from  JSON library alternative
-//$.getJSON("../../library/nounslist.json", function (data) {
-//   var arrItems = [];      // THE ARRAY TO STORE JSON ITEMS.
-//   $.each(data, function (index, value) {
-//       arrItems.push(value);    // PUSH THE VALUES INSIDE THE ARRAY.
-// });
-// });
-
-function text() {
-  var inputVal = document.getElementById("myInput").value;
-  document.getElementById("playerInput").value = inputVal;
-  document.getElementById("myInput").value = "";
-}
-
-$("#resetCanvas").click(function () {
- clearCanvas();
-});
-
-
-
-
-function download(){
-  document.getElementById("downloader").download = "image.png";
-  document.getElementById("downloader").href = document.getElementById("canvas").toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
-}
-
-var saveCanvas = function(){
-  var canvas = document.getElementById("canvas");
-  var d = canvas.toDataURL("image/png");
-  var w=window.open('about:blank','image from canvas');
-  w.document.write("<img src='"+d+"' alt='from canvas'/>");
-
-
-}
-window.addEventListener("DOMContentLoaded", function () {
-  var mouse = {
-    click: false,
-    move: false,
-    pos: { x: 0, y: 0 },
-    pos_prev: false,
-  };
-
-
-  // get canvas element and create context
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var socket = io.connect();
-
-  //image background 
-  document.getElementById("canvas").style.background = "url('images/art.jpg')";
-
-  // register mouse event handlers
-  canvas.onmousedown = function (e) {
-    mouse.click = true;
-  };
-  canvas.onmouseup = function (e) {
-    mouse.click = false;
-  };
-
-  canvas.onmousemove = function (e) {
-    //offsety and offsetx worked to corect position
-    mouse.pos.x = e.offsetX / width;
-    mouse.pos.y = e.offsetY / height;
-    mouse.move = true;
-  };
-
-  // draw line received from server
-  socket.on("draw_line", function (data) {
-    var line = data.line;
+// mouse draw function
+var draw = function(obj) {
+    context.fillStyle = obj.color;
     context.beginPath();
-    context.moveTo(line[0].x * width, line[0].y * height);
-    context.lineTo(line[1].x * width, line[1].y * height);
-    context.stroke();
-  });
+    context.arc(obj.position.x, obj.position.y,
+                    3, 0, 2 * Math.PI);
+                    context.shadowColor = '#999';
+                    context.shadowBlur = 20;
+                    context.shadowOffsetX = 15;
+                    context.shadowOffsetY = 15;               
+    context.fill();
+};
 
 
-  // main loop, running every 25ms
-  function mainLoop() {
-    // check if the user is drawing
-    if (mouse.click && mouse.move && mouse.pos_prev) {
-      // send line to to the server
-      socket.emit("draw_line", { line: [mouse.pos, mouse.pos_prev] });
-      mouse.move = false;
-    }
-    mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
-    setTimeout(mainLoop, 25);
-  }
-  mainLoop();
-});
+//display backgorund image
+document.getElementById("canvas").style.background = "url('images/canvas.jpg')";
 
-$(document).ready(function () {
-    clearCanvas();
+//function handling mouse operations
+var drawerMouse = function() {
+    clearScreen();
+    click = true;
+    console.log('draw status: ' + click);
+    $('#guess').hide();
+    $('#guesses').empty();
+    $('.draw').show();
 
-   //usernameAsk();
-  //saveCanvas();
-  newWord();
-  socket.on('clear screen', clearCanvas);
+    var drawing;
+    var color;
+    var obj = {};
+
+    $('.draw-buttons').on('click', 'button', function(){
+        obj.color = $(this).attr('value');
+        console.log(obj.color);
+
+        if (obj.color === '0') {
+            socket.emit('clear screen', user);
+            context.fillStyle = 'white';
+            return;
+        };
+    });
+
+    console.log('You are the drawer');
+
+    $('.users').on('dblclick', 'li', function() {
+        if (click == true) {
+            var target = $(this).text();
+            socket.emit('swap rooms', {from: user, to: target});
+        };
+    });
+
+    canvas.on('mousedown', function(event) { 
+        drawing = true;   
+    });
+    canvas.on('mouseup', function(event) {
+        drawing = false;
+    });
+
+    canvas.on('mousemove', function(event) {
+        var offset = canvas.offset();
+        obj.position = {x: event.pageX - offset.left,
+                        y: event.pageY - offset.top};
+        
+        if (drawing == true && click == true) {
+            draw(obj);
+            socket.emit('draw', obj);
+        };
+    });
+
+};
 
 
+//loads all functions
+$(document).ready(function() {
+    canvas = $('#canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+    usernameAsk();
+    socket.on('userlist', userlist);
+    socket.on('guesser', guesser);
+    socket.on('guessword', guessword);
+    socket.on('draw', draw);
+    socket.on('draw word', wordToDraw);
+    socket.on('drawer', drawerMouse);
+    socket.on('new drawer', newDrawer);
+    socket.on('correct answer', correctAnswer);
+    socket.on('reset', reset);
+    socket.on('clear screen', clearScreen);
 
 });
--- server.js
+++ server.js
@@ -1,46 +1,153 @@
-var express = require('express'), 
-    app = express(),
-    http = require('http'),
-    socketIo = require('socket.io');
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
 
-// start webserver on port 8080
-var server =  http.createServer(app);
-var io = socketIo.listen(server);
-server.listen(8000);
+var app = express();
+app.use(express.static('public'));
 
-// add directory with our static files
-app.use(express.static(__dirname + '/public'));
-console.log("Server running on 127.0.0.1:8000");
+var server = http.Server(app);
+var io = socket_io(server);
 
-// array of all lines drawn
-var line_history = [];
+var users = [];
 
-// event-handler for new incoming connections
-io.on('connection', function (socket) {
+var words = [
+	"school", "plant", "sun", "state", "eye", "city", "tree","farm","sea", 
+	"night","life","rock","fire", "problem", "airplane", "top", "king",
+    "space", "whale", "unicorn", "narwhal", "furniture","pigeon"
+];
+//random word from array.
+function newWord() {
+	wordcount = Math.floor(Math.random() * (words.length));
+	return words[wordcount];
+};
 
+var wordcount;
+io.on('connection', function (socket) 
+{
+	io.emit('userlist', users);
 
-   // first send the history to the new client
-   for (var i in line_history) {
-      socket.emit('draw_line', { line: line_history[i] } );
-   }
+	socket.on('join', function(name) {
+		socket.username = name;
 
+		// automatically joins with username
+		socket.join(name);
+		console.log(socket.username + ' has joined the room. USER ID: ' + socket.id);
 
+		// username to array
+		users.push(socket.username);
 
-   // add handler for message type "draw_line".
-   socket.on('draw_line', function (data) {
+		// if the player is first to join  with no players
+		if (users.length == 1 || typeof io.sockets.adapter.rooms['drawer'] === 'undefined') {
 
-      // add received line to history 
-      line_history.push(data.line);
+			// place player into 'drawer' room
+			socket.join('drawer');
 
-      // send line to all clients
-      io.emit('draw_line', { line: data.line });
-      //console.log('draw_line');
-   });
+			// server to user
+			io.in(socket.username).emit('drawer', socket.username);
+			console.log(socket.username + ' is a drawer');
 
-   socket.on('clear screen', function(data){
-       io.emit('clear screen',data);
-   })
+			// gets random word
+			io.in(socket.username).emit('draw word', newWord());
+				//console.log(socket.username + "'s + newWord());
+		} 
+		else {
 
+			// additional players
+			socket.join('guesser');
 
+			// server  to  the user
+			io.in(socket.username).emit('guesser', socket.username);
+			console.log(socket.username + ' is a guesser');
+		}
+	
+		// update all clients with the list of users
+		io.emit('userlist', users);
+		
+	});
+
+	// submit drawing on canvas to other clients
+	socket.on('draw', function(obj) {
+		socket.broadcast.emit('draw', obj);
+	});
+
+	// submit each client's guesses to all clients
+	socket.on('guessword', function(data) {
+		io.emit('guessword', { username: data.username, guessword: data.guessword})
+		console.log('guessword event triggered on server from: ' + data.username + ' with word: ' + data.guessword);
+	});
+
+	socket.on('disconnect', function() {
+		for (var i = 0; i < users.length; i++) {
+
+			// remove user from users list
+			if (users[i] == socket.username) {
+				users.splice(i, 1);
+			};
+		};
+		console.log(socket.username + ' has disconnected.');
+
+		//update list of players
+		io.emit('userlist', users);
+
+		//no connections
+		if ( typeof io.sockets.adapter.rooms['drawer'] === "undefined") {
+			
+			// generate random number based on length of users list
+			var x = Math.floor(Math.random() * (users.length));
+			console.log(users[x]);
+			io.in(users[x]).emit('new drawer', users[x]);
+		};
+	});
+
+	//new drawer actions
+	socket.on('new drawer', function(name) {
+
+		// remove user 
+		socket.leave('guesser');
+
+		// place player into room
+		socket.join('drawer');
+		console.log('new drawer emit: ' + name);
+
+		// drawer to player
+		socket.emit('drawer', name);
+		
+		// send a random word to the drawer
+		io.in('drawer').emit('draw word', newWord());
+	
+	});
+
+	socket.on('swap rooms', function(data) {
+
+		// drawer leaves 
+		socket.leave('drawer');
+		socket.join('guesser');
+
+		// player becomes drawer
+		socket.emit('guesser', socket.username);
+
+		// submit 'drawer' 
+		io.in(data.to).emit('drawer', data.to);
+
+		// new random word
+		io.in(data.to).emit('draw word', newWord());
+		io.emit('reset', data.to);
+
+	});
+
+	//when player guesses correct
+	socket.on('correct answer', function(data) {
+		io.emit('correct answer', data);
+		console.log(data.username + ' guessed correctly with ' + data.guessword);
+	});
+
+	//clear screen
+	socket.on('clear screen', function(name) {
+		io.emit('clear screen', name);
+	});
+
+})
+
+server.listen(process.env.PORT || 8000, function() {
+	console.log('Server started at http://localhost:8000');
 });
-
